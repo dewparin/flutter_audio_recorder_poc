fe2fad95fd9af8315d4c8878317a7fb47c05402a
Dew Parin <parinsura@gmail.com>
2021-08-17 13:52:42 +0700

    Prepare Android method channel.

--- android/app/src/main/kotlin/com/flutterpoc/flutter_audio_recorder_poc/MainActivity.kt
+++ android/app/src/main/kotlin/com/flutterpoc/flutter_audio_recorder_poc/MainActivity.kt
@@ -1,6 +1,26 @@
 package com.flutterpoc.flutter_audio_recorder_poc
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "com.flutterpoc.flutter_audio_recorder_poc/audio_recorder"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "start" -> {
+                    result.success(true)
+                }
+                "stop" -> {
+                    result.success(false)
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
 }
