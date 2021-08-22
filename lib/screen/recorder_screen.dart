d8891eb272040b72afec2c2a7984e56e0f3d2633
Dew Parin <parinsura@gmail.com>
2021-08-22 21:24:20 +0700

    Manage record/play states.

--- lib/screen/recorder_screen.dart
+++ lib/screen/recorder_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter_audio_recorder_poc/player.dart';
 import 'package:flutter_audio_recorder_poc/recorder.dart';
 import 'package:provider/provider.dart';
 
+enum _State { INITIAL, IDLE, RECORDING, PLAYING }
+
 const _mockWaveData = [
   34.0,
   49.0,
@@ -15,6 +17,24 @@ const _mockWaveData = [
 ];
 
 class RecorderScreen extends StatelessWidget {
+  bool _isInitialState = true;
+
+  _State _determineCurrentState(
+      {required bool isRecording, required bool isPlaying}) {
+    if (_isInitialState) {
+      _isInitialState = false;
+      return _State.INITIAL;
+    }
+
+    if (isRecording) {
+      return _State.RECORDING;
+    }
+    if (isPlaying) {
+      return _State.PLAYING;
+    }
+    return _State.IDLE;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +46,8 @@ class RecorderScreen extends StatelessWidget {
               builder: (context, recorder, player, child) {
                 final isRecording = recorder.isRecording;
                 final isPlaying = player.isPlaying;
+                final currentState = _determineCurrentState(
+                    isRecording: isRecording, isPlaying: isPlaying);
                 final status = recorder.statusMessage.isNotEmpty
                     ? recorder.statusMessage
                     : player.statusMessage;
@@ -35,20 +57,26 @@ class RecorderScreen extends StatelessWidget {
                     Container(
                       padding: EdgeInsets.all(8),
                       color: Colors.black12,
-                      child: isRecording
+                      child: currentState == _State.RECORDING
                           ? CircularProgressIndicator()
-                          : WaveformChart(data: _mockWaveData),
+                          : WaveformChart(
+                              data: _mockWaveData,
+                            ),
                     ),
                     OutlinedButton(
-                      child:
-                          isRecording ? Text("Stop Recording") : Text("Record"),
+                      child: currentState == _State.RECORDING
+                          ? Text("Stop Recording")
+                          : Text("Record"),
                       onPressed: isPlaying ? null : recorder.toggleRecorder,
                     ),
                     SizedBox(width: 8),
                     OutlinedButton(
-                      child: isPlaying ? Text("Stop Playing") : Text("Play"),
-                      // TODO: handle initial case where isRecording is false
-                      onPressed: isRecording ? null : player.togglePlayer,
+                      child: currentState == _State.PLAYING
+                          ? Text("Stop Playing")
+                          : Text("Play"),
+                      onPressed: currentState == _State.IDLE
+                          ? player.togglePlayer
+                          : null,
                     ),
                     Text(status),
                   ],
